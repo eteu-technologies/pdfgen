cc9b5f42a6f51b03422df54ddfcee4b09da5c7f8
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2021-08-31 18:44:44 +0300

    Add Docker image CI build/push script

---
+++ docker/ci.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+set -euo pipefail
+: "${DOCKER_REGISTRY:=docker.io}"
+: "${DOCKER_REPOSITORY:=eteu/pdfgen}"
+: "${IMAGE_COMMIT_TAG:=commit-"$(git rev-parse HEAD || echo "0000000000000000000000000000000000000000")"}"
+
+built_imgs=()
+for f in ./docker/Dockerfile.*; do
+    f="$(basename -- "${f}")"
+    arch="${f/Dockerfile\./}"
+    ./docker/build_docker.sh "${arch}" --load || {
+        echo ">>> Failed to build image for arch '${arch}'"
+        exit 1
+    }
+
+    # tag image with git commit id & latest tags
+    tag="${DOCKER_REGISTRY}/${DOCKER_REPOSITORY}:${IMAGE_COMMIT_TAG}-${arch}"
+    docker image tag "eteu/pdfgen:${arch}" "${tag}"
+    built_imgs+=("${tag}")
+done
+
+# push all the images
+for img in "${built_imgs[@]}"; do
+    docker image push "${img}"
+done
+
+# create a manifrst
+docker manifest create --insecure "${DOCKER_REGISTRY}/${DOCKER_REPOSITORY}:latest" \
+    "${built_imgs[@]}"
+docker manifest create --insecure "${DOCKER_REGISTRY}/${DOCKER_REPOSITORY}:${IMAGE_COMMIT_TAG}" \
+    "${built_imgs[@]}"
+
+# push
+docker manifest push --purge --insecure "${DOCKER_REGISTRY}/${DOCKER_REPOSITORY}:latest"
+docker manifest push --purge --insecure "${DOCKER_REGISTRY}/${DOCKER_REPOSITORY}:${IMAGE_COMMIT_TAG}"
