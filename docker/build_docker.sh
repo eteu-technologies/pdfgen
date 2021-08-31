17e9a560fffeca9bf23fc782b1a260e437eb1c62
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2021-08-31 17:54:36 +0300

    Add Docker build scripts

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -1,11 +1,45 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-export PROJ_VERSION="$(git describe --tags --abbrev 2>/dev/null || echo "0.0.0-dev")"
-export DOCKER_BUILDKIT=1
+export PROJ_VERSION="$(git rev-parse HEAD || echo "0000000000000000000000000000000000000000")"
 
-docker build \
+arch="${1:-}"
+hostarch="$(uname -m)"
+if [ -z "${arch:-}" ]; then
+	arch="$(uname -m)"
+else
+	shift
+fi
+
+fixup_arch () {
+	case "${1}" in
+		x86_64)
+			echo "amd64"
+			;;
+		armv8*)
+			echo "arm64"
+			;;
+		aarch64)
+			echo "arm64"
+			;;
+		*)
+			echo "${1}"
+			;;
+	esac
+}
+
+arch="$(fixup_arch "${arch}")"
+hostarch="$(fixup_arch "${hostarch}")"
+
+if ! [ -f docker/Dockerfile."${arch}" ]; then
+	echo "'docker/Dockerfile."${arch}"' does not exist"
+	exit 1
+fi
+
+docker buildx build \
+	--platform "${arch}" \
+	--build-arg BUILDPLATFORM="${hostarch}" \
         --build-arg PROJ_VERSION="${PROJ_VERSION}" \
 	"${@}" \
-        -t eteu/pdfgen \
-        -f docker/Dockerfile .
+        -t eteu/pdfgen:"${arch}" \
+        -f docker/Dockerfile."${arch}" .
